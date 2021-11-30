7384990ef550d12ccfe2d1be4e5c18aa73f3ae8f
Adrian <szmiteek1994@gmail.com>
2021-11-30 13:40:45 +0100

    first update

--- src/components/Header.js
+++ src/components/Header.js
@@ -3,10 +3,6 @@ import "../styles/Header.css";
 import header from "../images/header.png";
 
 const Header = () => {
-  return (
-    <header>
-      <img src={header} alt="logo" />
-    </header>
-  );
+  return <header>{/* <img src={header} alt="logo" /> */}</header>;
 };
 export default Header;
